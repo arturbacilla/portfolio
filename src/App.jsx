80c39504994cc0ec24bb0803da57c1c0849d5df7
arturbacilla <artur.bacilla1@gmail.com>
2022-07-05 20:51:08 -0300

    :sparkles: Added landing page
    Added landing page

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { ReactComponent as Me } from './assets/dev_me.svg';
+
+function App() {
+  return (
+    <main>
+      <section className="presentation-text">
+        <p className="bold-paragraph">
+          Oi, eu sou o Artur.
+          Eu sou Desenvolvedor Web Fullstack e Engenheiro Mecânico
+        </p>
+        <p>
+          Esse site está em desenvolvimento
+          para me apresentar e mostrar um pouco das minhas habilidades.
+        </p>
+      </section>
+      <Me className="landing-img" />
+    </main>
+  );
+}
+
+export default App;
